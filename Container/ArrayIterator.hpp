92cc8f98cb3d30451a1aaf24a8a3f3aeb839f1fe
Matt <cygnusvis@gmail.com>
2017-03-15 12:14:44 -0700

    Cleaned up the header for the ArrayIterator and DoubleLinkiterator.  Both of those iterators work properly now.

--- Container/ArrayIterator.hpp
+++ Container/ArrayIterator.hpp
@@ -2,7 +2,7 @@
 
 #include "Iterator.hpp"
 
-#define _DEBUGAI _DEBUG
+#define _DEBUGAI _DEBUG && 1
 
 namespace cg {
 
@@ -20,228 +20,116 @@ public:
 		: m_ptr(ptr) {}
 	/**Copy asignment contructor.
 	\param other The other thing to copy.*/
-	ArrayIterator(const self_type& other)
-		:m_ptr(other.m_ptr)
-	{
-#if defined(_DEBUGAI)
-
-		m_beforeFirst = other.m_beforeFirst;
-		m_afterLast = other.m_afterLast;
-#endif
-	}
+	ArrayIterator(const self_type& other);
 	/**Move asignment contructor.
 	\param other The other thing to move.*/
-	ArrayIterator(self_type&& other)
-		:m_ptr(std::move(other.m_ptr))
-	{
-#if defined(_DEBUGAI)
-
-		m_beforeFirst = other.m_beforeFirst;
-		m_afterLast = other.m_afterLast;
-#endif
-	}
-#if defined(_DEBUGAI)
-	inline void DebugBounds(pointer beforeFirst, pointer afterLast)
-	{
-		m_beforeFirst = beforeFirst;
-		m_afterLast = afterLast;
-	}
-#endif
+	ArrayIterator(self_type&& other);
 	/**Copy assignment operator.
 	\param other The other iterator.*/
-	inline void operator=(const self_type& other)
-	{
-		m_ptr = other.m_ptr;
-#if defined(_DEBUGAI)
-		m_beforeFirst = other.m_beforeFirst;
-		m_afterLast = other.m_afterLast;
-#endif
-	}
+	inline void operator=(const self_type& other);
 	/**Move assignment operator.
 	\param other The other iterator.*/
-	inline void operator=(self_type&& other)
-	{
-		m_ptr = std::move(other.m_ptr);
-#if defined(_DEBUGAI)
-		m_beforeFirst = std::move(other.m_beforeFirst);
-		m_afterLast = std::move(other.m_afterLast);
-#endif
-	}
+	inline void operator=(self_type&& other);
 	/**Increment the poitner by 1. (prefix)
 	\return This same type.*/
-	inline self_type& operator++()
-	{
-		++m_ptr;
-#if defined(_DEBUGAI)
-		CheckForStomp();
-#endif
-		return *this;
-	}
+	inline self_type& operator++();
 	/**Increment this by 1. (postfix)
 	\param nothing Ignored pointer.
 	\return a copy of this befor incrementing.*/
-	inline self_type operator++(int nothing)
-	{
-		auto copy = *this;
-		++(*this);
-		return copy;
-	}
+	inline self_type operator++(int nothing);
 	/**Decrement the poitner by 1. (prefix)
 	\return This same type after incrementing.*/
-	inline self_type& operator--()
-	{
-		--m_ptr;
-#if defined(_DEBUGAI)
-		CheckForStomp();
-#endif
-		return *this;
-	}
+	inline self_type& operator--();
 	/**Decrement this by 1. (postfix)
 	\param nothing Ignored pointer.
 	\return a copy of this before decrementing.*/
-	inline self_type operator--(int nothing)
-	{
-		auto copy = *this;
-		--(*this);
-		return copy;
-	}
+	inline self_type operator--(int nothing);
 	/**Add to this node.
 	\param amt The amount to increment.
 	\return A reference to this.*/
-	inline self_type& operator+=(std::size_t amt)
-	{
-		m_ptr += amt;
-#if defined(_DEBUGAI)
-		CheckForStomp();
-#endif
-		return *this;
-	}
+	inline self_type& operator+=(std::size_t amt);
 	/**Sub from this node.
 	\param amt The amount to decrement.
 	\return A reference to this.*/
-	inline self_type& operator-=(std::size_t amt)
-	{
-		m_ptr -= amt;
-#if defined(_DEBUGAI)
-		CheckForStomp();
-#endif
-		return *this;
-	}
+	inline self_type& operator-=(std::size_t amt);
 	/**The plus operator.
 	\param amt The amount to add to the position.*/
-	inline self_type operator+(std::size_t amt) const
-	{
+	inline self_type operator+(std::size_t amt) const{
 		auto copy = *this;
 		return copy += amt;
 	}
 	/**The sub operator.
 	\param amt The amount to sub from the position.*/
-	inline difference_type operator-(std::size_t amt) const
-	{
+	inline difference_type operator-(std::size_t amt) const{
 		return m_ptr - amt;
 	}
 	/**The sub operator.
 	\param ptr The ptr to sub from this.*/
-	inline difference_type operator-(pointer ptr) const
-	{
+	inline difference_type operator-(pointer ptr) const{
 		return m_ptr - ptr;
 	}
 	/**The sub operator.
 	\param other The object to sub from the position.
 	\return The space between the this iterator and another.*/
-	inline difference_type operator-(const self_type& other) const
-	{
-#if defined(_DEBUGAI)
-		CheckValid();
-		other.CheckValid();
-#endif
-		return m_ptr - other.m_ptr;
-	}
+	inline difference_type operator-(const self_type& other) const;
 	/**Dereference operator.
 	\return A reference to the data contained.*/
-	inline RefReturnType operator*()
-	{
+	inline RefReturnType operator*(){
 		return *(this->operator->());
 	}
 	/**Poitner member access.
 	\return A pointer to this (-> is appolied to the pointer too).*/
-	inline PointerReturnType operator->()
-	{
-#if defined(_DEBUGAI)
-		CheckValid();
-#endif
-		return m_ptr;
-	}
+	inline PointerReturnType operator->();
 	/**Compare for equality.
 	\return True if the iterators are the same.*/
-	inline bool operator==(const self_type& rhs) const
-	{
+	inline bool operator==(const self_type& rhs) const{
 		return m_ptr == rhs.m_ptr;
 	}
 	/**Compare for equality.
 	\return False if the iterators are the same.*/
-	inline bool operator!=(const self_type& rhs) const
-	{
+	inline bool operator!=(const self_type& rhs) const{
 		return !(*this == rhs);
 	}
 	/**Compare less than.
 	\param other The other iterator to compare.
 	\return True if other is before this in memory.*/
-	inline bool operator<(const self_type& other) const
-	{
+	inline bool operator<(const self_type& other) const{
 		return m_ptr < other.m_ptr;
 	}
 	/**Greater than comparison.
 	\param other The other thing to compare.
 	\return True if the other thing is after this in memory.*/
-	inline bool operator>(const self_type& other) const
-	{
+	inline bool operator>(const self_type& other) const{
 		return !((*this) <= other);
 	}
 	/**Compare less than or equals.
 	\param other The other iterator to compare.
 	\return True if other is before this in memory or at the same place.*/
-	inline bool operator<=(const self_type& other) const
-	{
+	inline bool operator<=(const self_type& other) const{
 		return (*this < other) || (*this == other);
 	}
 	/**Greater than or equals comparison.
 	\param other The other thing to compare.
 	\return True if the other thing is after this in memory or at the same
 	place.*/
-	inline bool operator>=(const self_type& other) const
-	{
+	inline bool operator>=(const self_type& other) const{
 		return !(*this < other);
 	}
 private:
+	/**Global plus operator for STL compatibility.*/
 	template<typename T, bool Const>
 	friend std::ptrdiff_t operator+(std::size_t,
 		const ArrayIterator<T, Const>&);
 
-#if defined(_DEBUGAI)
-	pointer m_beforeFirst = nullptr;
-	pointer m_afterLast = nullptr;
+#if _DEBUGAI
+	pointer md_beforeFirst = nullptr;
+	pointer md_afterLast = nullptr;
 	/**Throw an error if the pointer is in an invalid location.*/
-	void CheckForStomp() const
-	{
-		if (m_beforeFirst && m_afterLast)
-			if (m_ptr < m_beforeFirst || m_ptr > m_afterLast)
-				throw IteratorOutOfBoundsException();
-
-		if (!m_ptr)
-			throw NullPtrException();
-	}
+	void D_CheckForStomp() const;
 	/**Throw an error if the poitner is not dereferencable.*/
-	void CheckValid() const
-	{
-		if (m_beforeFirst && m_afterLast)
-			if (m_ptr <= m_beforeFirst
-				|| m_ptr >= m_afterLast)
-				throw IteratorOutOfBoundsException();
-
-		if (!m_ptr)
-			throw NullPtrException();
-	}
+	void D_CheckValid() const;
+	/**Set the debug members for the bounds.*/
+	inline void D_DebugBounds(pointer beforeFirst, pointer afterLast);
 #endif
 	/**A pointer to the data.*/
 	pointer m_ptr;
@@ -254,5 +142,182 @@ operator+(std::size_t lhs, const ArrayIterator<T, Const>& rhs)
 	return rhs.m_ptr + lhs;
 }
 
+
+template<typename T, bool Const>
+inline ArrayIterator<T, Const>::ArrayIterator(const self_type & other)
+	:m_ptr(other.m_ptr)
+{
+#if _DEBUGAI
+
+	md_beforeFirst = other.md_beforeFirst;
+	md_afterLast = other.md_afterLast;
+#endif
+}
+
+template<typename T, bool Const>
+inline ArrayIterator<T, Const>::ArrayIterator(self_type && other)
+	:m_ptr(std::move(other.m_ptr))
+{
+#if _DEBUGAI
+
+	md_beforeFirst = other.md_beforeFirst;
+	md_afterLast = other.md_afterLast;
+#endif
+}
+
+template<typename T, bool Const>
+inline void ArrayIterator<T, Const>::operator=(const self_type & other)
+{
+	m_ptr = other.m_ptr;
+#if _DEBUGAI
+	md_beforeFirst = other.md_beforeFirst;
+	md_afterLast = other.md_afterLast;
+#endif
+}
+
+template<typename T, bool Const>
+inline void ArrayIterator<T, Const>::operator=(self_type && other)
+{
+	m_ptr = std::move(other.m_ptr);
+#if _DEBUGAI
+	md_beforeFirst = std::move(other.md_beforeFirst);
+	md_afterLast = std::move(other.md_afterLast);
+#endif
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type &
+ArrayIterator<T, Const>::operator++()
+{
+	++m_ptr;
+#if _DEBUGAI
+	D_CheckForStomp();
+#endif
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type
+ArrayIterator<T, Const>::operator++(int nothing)
+{
+	auto copy = *this;
+	++(*this);
+	return copy;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type &
+ArrayIterator<T, Const>::operator--()
+{
+	--m_ptr;
+#if _DEBUGAI
+	D_CheckForStomp();
+#endif
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type
+ArrayIterator<T, Const>::operator--(int nothing)
+{
+	auto copy = *this;
+	--(*this);
+	return copy;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type &
+ArrayIterator<T, Const>::operator+=(std::size_t amt)
+{
+	m_ptr += amt;
+#if _DEBUGAI
+	D_CheckForStomp();
+#endif
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::self_type &
+ArrayIterator<T, Const>::operator-=(std::size_t amt)
+{
+	m_ptr -= amt;
+#if _DEBUGAI
+	D_CheckForStomp();
+#endif
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::difference_type
+ArrayIterator<T, Const>::operator-(const self_type & other) const
+{
+#if _DEBUGAI
+	D_CheckValid();
+	other.D_CheckValid();
+#endif
+	return m_ptr - other.m_ptr;
+}
+
+template<typename T, bool Const>
+inline typename ArrayIterator<T, Const>::PointerReturnType
+ArrayIterator<T, Const>::operator->()
+{
+#if _DEBUGAI
+	D_CheckValid();
+#endif
+	return m_ptr;
+}
+
+
+#if _DEBUGAI
+template<typename T, bool Const>
+inline void ArrayIterator<T, Const>::D_DebugBounds(pointer beforeFirst,
+	pointer afterLast)
+{
+	md_beforeFirst = beforeFirst;
+	md_afterLast = afterLast;
+}
+template<typename T, bool Const>
+inline void ArrayIterator<T, Const>::D_CheckForStomp() const
+
+{
+	if (md_beforeFirst && md_afterLast)
+		if (m_ptr < md_beforeFirst || m_ptr > md_afterLast)
+		{
+			cg::Logger::LogError(__FUNCSTR__,
+				"The iterator is out of bounds.");
+			throw IteratorOutOfBoundsException();
+		}
+
+	if (!m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator does not container a valid data pointer.");
+		throw NullPtrException();
+
+	}
+}
+template<typename T, bool Const>
+inline void ArrayIterator<T, Const>::D_CheckValid() const
+
+{
+	if (md_beforeFirst && md_afterLast)
+		if (m_ptr <= md_beforeFirst
+			|| m_ptr >= md_afterLast)
+		{
+			cg::Logger::LogError(__FUNCSTR__,
+				"The iterator is out of bounds.");
+			throw IteratorOutOfBoundsException();
+		}
+
+	if (!m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator does not container a valid data pointer.");
+		throw NullPtrException();
+
+	}
+}
+#endif
 
 }
--- Container/DoubleLinkIterator.hpp
+++ Container/DoubleLinkIterator.hpp
@@ -2,7 +2,7 @@
 
 #include "Iterator.hpp"
 
-#define _DEBUGDLI _DEBUG
+#define _DEBUGDLI _DEBUG && 1
 
 namespace cg {
 
@@ -16,19 +16,10 @@ public:
 		first and lost nodes in a chain.
 		\param beforeBegin The very first node.
 		\param afterEnd The last node.*/
-		static void CreateInitialPair(Node& beforeBegin, Node& afterEnd)
-		{
-			beforeBegin.m_prev = nullptr;
-			beforeBegin.m_ptr = nullptr;
-			beforeBegin.m_next = &afterEnd;
-			afterEnd.m_ptr = nullptr;
-			afterEnd.m_prev = &beforeBegin;
-			afterEnd.m_next = nullptr;
-#if defined(_DEBUGDLI)
-			beforeBegin.md_beforeFirst = true;
-			afterEnd.md_afterLast = true;
-#endif
-		}
+		static void CreateLinkedEnds(Node& beforeBegin, Node& afterEnd);
+		/**Delete the data pointers. If before_begin node is deleted,
+		than the whole chain gets deleted.*/
+		~Node();
 		/**A pointer to the data.*/
 		pointer m_ptr;
 		/**A link to the next iterator.*/
@@ -37,23 +28,25 @@ public:
 		Node* m_prev;
 		/**Determine if this is the before_first node.
 		\return True if this node exists before the first value node.*/
-		bool IsBeforeFirst() const
-		{
+		bool IsBeforeFirst() const {
 			return !m_ptr && !m_prev && m_next;
 		}
 		/**Determine if this is the after_last node.
 		\return True if this node exists after the last value node.*/
-		bool IsAfterLast() const
-		{
+		bool IsAfterLast() const {
 			return !m_ptr && m_prev && !m_next;
 		}
-#if defined(_DEBUGDLI)
+#if _DEBUGDLI
 		/**Debug only member to see the address of this object.*/
 		const Node* md_self = this;
 		/**Debug member for knowing if this is the before first.*/
 		bool md_beforeFirst = false;
 		/**Debug member for knowing if this is the after last.*/
 		bool md_afterLast = false;
+		/**Throw if trying to move past an cap iterator.*/
+		void D_CheckBounds()const;
+		/**Throw an error if the poitner is not dereferencable.*/
+		void D_CheckValid() const;
 #endif
 	};
 	/**The same type as this object.*/
@@ -64,68 +57,41 @@ public:
 	\param ptr THe pointer to the data.
 	\param next The pointer to the next link.
 	\param prev The previous link in the chain.*/
-	DoubleLinkIterator(Node* node)
-	{
+	DoubleLinkIterator(Node* node) {
 		m_node = node;
 	}
 	/**Copy asignment contructor.
 	\param other The other thing to copy.*/
 	DoubleLinkIterator(const self_type& other)
-		:m_node(other.m_node){}
+		:m_node(other.m_node) {}
 	/**Move asignment contructor.
 	\param other The other thing to move.*/
 	DoubleLinkIterator(self_type&& other)
-		:m_node(std::move(other.m_node)){}
+		:m_node(std::move(other.m_node)) {}
 	/**Copy assignment operator.
 	\param other The other iterator.*/
-	inline void operator=(const self_type& other)
-	{
+	inline void operator=(const self_type& other) {
 		m_node = other.m_node;
 	}
 	/**Move assignment operator.
 	\param other The other iterator.*/
-	inline void operator=(self_type&& other)
-	{
+	inline void operator=(self_type&& other) {
 		m_node = std::move(other.m_node);
 	}
 	/**Increment the poitner by 1. (prefix)
 	\return This same type.*/
-	inline self_type& operator++()
-	{
-#if defined(_DEBUGDLI)
-		CheckBounds();
-#endif
-		m_node = m_node->m_next;
-		return *this;
-	}
+	inline self_type& operator++();
 	/**Increment this by 1. (postfix)
 	\param nothing Ignored pointer.
 	\return a copy of this befor incrementing.*/
-	inline self_type operator++(int nothing)
-	{
-		auto copy = *this;
-		++(*this);
-		return copy;
-	}
+	inline self_type operator++(int nothing);
 	/**Decrement the poitner by 1. (prefix)
 	\return This same type after incrementing.*/
-	inline self_type& operator--()
-	{
-#if defined(_DEBUGDLI)
-		CheckBounds();
-#endif
-		m_node = m_node->m_prev;
-		return *this;
-	}
+	inline self_type& operator--();
 	/**Decrement this by 1. (postfix)
 	\param nothing Ignored pointer.
 	\return a copy of this before decrementing.*/
-	inline self_type operator--(int nothing)
-	{
-		auto copy = *this;
-		--(*this);
-		return copy;
-	}
+	inline self_type operator--(int nothing);
 	/**Dereference operator.
 	\return A reference to the data contained.*/
 	inline RefReturnType operator*()
@@ -134,75 +100,270 @@ public:
 	}
 	/**Poitner member access.
 	\return A pointer to this (-> is appolied to the pointer too).*/
-	inline PointerReturnType operator->()
-	{
-#if defined(_DEBUGDLI)
-		CheckValid();
-#endif
-		return m_node->m_ptr;
-	}
+	inline PointerReturnType operator->();
 	/**Compare for equality.
 	\return True if the iterators are the same.*/
-	inline bool operator==(const self_type& rhs) const
-	{
+	inline bool operator==(const self_type& rhs) const {
 		return m_node == rhs.m_node;
 	}
 	/**Compare for equality.
 	\return False if the iterators are the same.*/
-	inline bool operator!=(const self_type& rhs) const
-	{
+	inline bool operator!=(const self_type& rhs) const {
 		return !(*this == rhs);
 	}
 	/**Add a new link right after this one.
 	\param v A value to copy or move to the new node data location.
 	\return An iterator for the newly inserted object.*/
-	self_type InsertAfter(value_type v)
-	{
-		if (m_node->IsAfterLast())
-		{
-			throw IteratorOutOfBoundsException();
-		}
-		auto newNode = cg::New<Node>();
-		newNode->m_ptr = cg::New<value_type>(v);
-		newNode->m_prev = m_node;
-		newNode->m_next = m_node->m_next;
-		m_node->m_next = newNode;
-		return self_type(newNode);
-	}
+	self_type InsertAfter(value_type v);
 	/**Add a new link right before this one.
 	\param v A value to copy or move to the new node data location.
 	\return An iterator for the newly inserted object.*/
-	self_type InsertBefore(value_type v)
-	{
-		if (m_node->IsBeforeFirst())
-		{
-			throw IteratorOutOfBoundsException();
-		}
-		auto newNode = cg::New<Node>();
-		newNode->m_ptr = cg::New<value_type>(v);
-		newNode->m_next = m_node;
-		newNode->m_prev = m_node->m_prev;
-		m_node->m_prev = newNode;
-		return self_type(newNode);
+	self_type InsertBefore(value_type v);
+	/**A lexigraphic function. InsertBefore is the same. The index that the
+	iterator is currently on, will be the index for the new variable.
+	\sa InsertBefore
+	\param v The value to be inserted.*/
+	self_type InsertHere(value_type v) {
+		return InsertBefore(v);
 	}
+	/**Print the chain from the next link to the last link. The best way to do
+	it is to call print on the beforeBegin iterator.
+	\param strm A stream buffer to print to.*/
+	void PrintChain(std::streambuf* strm);
+	/**Print the entire chain from the previous link to the first link. The
+	best way to do it is to call print on the afterEnd iterator.
+	\param strm A stream buffer to print to.*/
+	void PrintChainReverse(std::streambuf* strm);
 private:
 
-#if defined(_DEBUGDLI)
+#if _DEBUGDLI
 	/**Throw if trying to move past an cap iterator.*/
-	void CheckBounds()
-	{
-		if (!m_node->m_ptr)
-			throw IteratorOutOfBoundsException();
-	}
+	void D_CheckBounds()const;
 	/**Throw an error if the poitner is not dereferencable.*/
-	void CheckValid() const
-	{
-		if (!m_node->m_ptr)
-			throw NullPtrException();
-	}
+	void D_CheckValid() const;
 #endif
+	/**A pointer to the node for this iterator.*/
 	Node* m_node;
 };
 
+
+template<typename T, bool Const>
+inline void
+DoubleLinkIterator<T, Const>::PrintChain(std::streambuf * strm)
+{
+	std::ostream out(strm);
+	std::size_t index = 0;
+	for (Node* n = m_node->m_next; n != nullptr; n = n->m_next)
+	{
+		if (!n->m_ptr)
+		{
+			out << std::endl;
+			break;
+		}
+		out << "\n" << "[" << ++index << "]= " << *n->m_ptr;
+	}
+}
+
+template<typename T, bool Const>
+inline void
+DoubleLinkIterator<T, Const>::PrintChainReverse(std::streambuf * strm)
+{
+	std::ostream out(strm);
+	std::size_t index = 0;
+	for (Node* n = m_node->m_prev; n != nullptr; n = n->m_prev)
+	{
+		if (!n->m_ptr)
+		{
+			out << std::endl;
+			break;
+		}
+		out << "\n" << "[" << ++index << "]= " << *n->m_ptr;
+	}
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type &
+DoubleLinkIterator<T, Const>::operator++()
+{
+#if _DEBUGDLI
+	D_CheckBounds();
+#endif
+	m_node = m_node->m_next;
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type
+DoubleLinkIterator<T, Const>::operator++(int nothing)
+{
+	auto copy = *this;
+	++(*this);
+	return copy;
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type &
+DoubleLinkIterator<T, Const>::operator--()
+{
+#if _DEBUGDLI
+	D_CheckBounds();
+#endif
+	m_node = m_node->m_prev;
+	return *this;
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type
+DoubleLinkIterator<T, Const>::operator--(int nothing)
+{
+	auto copy = *this;
+	--(*this);
+	return copy;
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::PointerReturnType
+DoubleLinkIterator<T, Const>::operator->()
+{
+#if _DEBUGDLI
+	D_CheckValid();
+#endif
+	return m_node->m_ptr;
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type
+DoubleLinkIterator<T, Const>::InsertAfter(value_type v)
+{
+#if _DEBUGDLI
+	if (m_node->IsAfterLast())
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator is out of bounds.");
+		throw IteratorOutOfBoundsException();
+
+	}
+#endif
+	auto newNode = cg::New<Node>();
+	/*create data space*/
+	newNode->m_ptr = cg::New<value_type>(v);
+	/*my old next's previous = the new node*/
+	m_node->m_next->m_prev = newNode;
+	/*new node prev = me*/
+	newNode->m_prev = m_node;
+	/*new node nex = my old next*/
+	newNode->m_next = m_node->m_next;
+	/*my next = new node*/
+	m_node->m_next = newNode;
+	return self_type(newNode);
+}
+
+template<typename T, bool Const>
+inline typename DoubleLinkIterator<T, Const>::self_type
+DoubleLinkIterator<T, Const>::InsertBefore(value_type v)
+{
+#if _DEBUGDLI
+	if (m_node->IsBeforeFirst())
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator is out of bounds.");
+		throw IteratorOutOfBoundsException();
+
+	}
+#endif
+	auto newNode = cg::New<Node>();
+	/*set new data ptr*/
+	newNode->m_ptr = cg::New<value_type>(v);
+	/*new node next = me*/
+	newNode->m_next = m_node;
+	/*new node prev = my old prev*/
+	newNode->m_prev = m_node->m_prev;
+	/*my old previous' next = the new node*/
+	m_node->m_prev->m_next = newNode;
+	/*my prev = new node*/
+	m_node->m_prev = newNode;
+	return self_type(newNode);
+}
+
+
+
+#if _DEBUGDLI
+template<typename T, bool Const>
+inline void DoubleLinkIterator<T, Const>::D_CheckBounds() const
+{
+	if (!m_node->m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator is out of bounds.");
+		throw IteratorOutOfBoundsException();
+	}
+}
+template<typename T, bool Const>
+inline void DoubleLinkIterator<T, Const>::D_CheckValid() const
+{
+	if (!m_node->m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator does not have a valid data pointer.");
+		throw NullPtrException();
+	}
+}
+#endif
+
+
+
+/*****************************************************************************/
+
+template<typename T, bool Const>
+inline void DoubleLinkIterator<T, Const>::Node::CreateLinkedEnds(
+	Node & beforeBegin,
+	Node & afterEnd)
+{
+	beforeBegin.m_prev = nullptr;
+	beforeBegin.m_ptr = nullptr;
+	beforeBegin.m_next = &afterEnd;
+	afterEnd.m_ptr = nullptr;
+	afterEnd.m_prev = &beforeBegin;
+	afterEnd.m_next = nullptr;
+#if _DEBUGDLI
+	beforeBegin.md_beforeFirst = true;
+	afterEnd.md_afterLast = true;
+#endif
+}
+
+template<typename T, bool Const>
+inline DoubleLinkIterator<T, Const>::Node::~Node()
+{
+	if (!IsBeforeFirst() && !IsAfterLast())
+	{
+#if _DEBUGDLI
+		D_CheckValid();
+#endif
+		cg::Delete(m_ptr);
+	}
+}
+
+#if _DEBUGDLI
+template<typename T, bool Const>
+inline void DoubleLinkIterator<T, Const>::Node::D_CheckBounds() const
+{
+	if (!m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator is out of bounds.");
+		throw IteratorOutOfBoundsException();
+	}
+}
+template<typename T, bool Const>
+inline void DoubleLinkIterator<T, Const>::Node::D_CheckValid() const
+{
+	if (!m_ptr)
+	{
+		cg::Logger::LogError(__FUNCSTR__,
+			"The iterator does not have a valid data pointer.");
+		throw NullPtrException();
+	}
+}
+#endif
 
 }
